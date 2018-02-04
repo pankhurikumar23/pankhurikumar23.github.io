2aeef475e461faacaa8f6b9d00565682d863be39
Pankhuri Kumar <pankhurikumar23@gmail.com>
2018-02-03 23:24:50 -0500

    standardizing code

--- A2/flora.js
+++ A2/flora.js
@@ -5,9 +5,9 @@ function setup() {
 
 function draw() {
 	background('#D8A0AE');
-	reset_seconds();
-  	reset_minutes();
-  	reset_hours();
+	reset(60, 800);
+  	reset(60, 450);
+  	reset(24, 100);
 
   	var s = second();
   	var m = minute();
@@ -37,9 +37,9 @@ function draw() {
   		drawTime(i, '#5D001E', 24, 100, height/24);
   	}
 
-  	if (s == 0) {reset_seconds();}
-  	if (m == 0	) {reset_minutes();}
-  	if (h == 0) {reset_hours();}
+  	if (s == 0) {reset(60, 800);}
+  	if (m == 0) {reset(60, 450);}
+  	if (h == 0) {reset(24, 100);}
 
 }
 
@@ -49,33 +49,11 @@ function drawTime(i, color, original_max, starting_point, s_dist) {
   	line(starting_point, (i+1)*s_dist, starting_point+100, (i+1)*s_dist);
 }
 
-function reset_seconds() {
-  var s_dist = height/60;
+function reset(max_value, starting_point) {
+  var s_dist = height/max_value;
   stroke('#E3E2DF');
   strokeWeight(2);
-  for (var i = 0; i < 60; i++) {
-  	line(800, (i+1)*s_dist, 900, (i+1)*s_dist);
-  	// textSize(10);
-  	// text(i+1, 190, (i+1)*s_dist);
+  for (var i = 0; i < max_value; i++) {
+  	line(starting_point, (i+1)*s_dist, starting_point+100, (i+1)*s_dist);
   }
-}
-
-function reset_minutes() {
-	var s_dist = height/60;
-  	stroke('#E3E2DF');
-  	for (var i = 0; i < 60; i++) {
-		line(450, (i+1)*s_dist, 550, (i+1)*s_dist);
-  		// textSize(10);
-  		// text(i+1, 190, (i+1)*s_dist);
-  	}
-}
-
-function reset_hours() {
-	var s_dist = height/24;
-  	stroke('#E3E2DF');
-  	for (var i = 0; i < 24; i++) {
-		line(100, (i+1)*s_dist, 200, (i+1)*s_dist);
-  		// textSize(10);
-  		// text(i+1, 190, (i+1)*s_dist);
-  	}
 }
