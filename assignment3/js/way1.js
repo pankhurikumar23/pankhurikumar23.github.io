32f3895e47088b3ed09f21940525fd1d4888e908
Pankhuri Kumar <pankhurikumar23@gmail.com>
2018-02-12 20:33:24 -0500

    visualization 1

--- assignment3/js/way1.js
+++ assignment3/js/way1.js
@@ -1,20 +1,24 @@
 var response = [];
-var results = [];
 var datapoints = {};
 var headlines = {};
+var topK = {};
+
+var defaultTextSize = 10;
+var maxTextSize = 95;
+var maxValue;
 
 function preload() {
 
   var baseURL = "https://api.nytimes.com/svc/topstories/v2/";
   var apikey = "f7a0f1f8f99d401594f60a00d96c4629";
-  var sections = ["home", "opinion", "world", "national", "politics", "upshot", "nyregion", "business", "technology", "science", "health", "sports", "arts", "books", "movies", "theater", 
-  "sundayreview", "fashion", "tmagazine", "food", "travel", "magazine", "realestate", "automobiles", "obituaries", "insider"];
+  var sections = ["opinion", "world", "politics", "upshot", "business", "technology", "science", "health", "sports", "arts", 
+   "fashion", "food", "travel", "magazine", "insider"];
 
   for (var i = 0; i < sections.length; i++) {
     url = baseURL + sections[i] + ".json?api-key=" + apikey;
     response.push(loadJSON(url));
     // time loop to prevent hitting API rate limits
-    for (var x= 0; x < 1100000000; x++);
+    for (var x= 0; x < 2000000000; x++);
     // keeping track of number of queries in console
     console.log("Querying " + (i+1) + " out of " + (sections.length) + " resources.");
   }
@@ -22,21 +26,82 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1907, 3000);
+  createCanvas(1907, 2100);
+  background('#282726');
 
-  background('grey');
-  noLoop();
+  textSize(defaultTextSize);
+  textAlign(CENTER);
+  noLoop(); 
 
+  //organize articles into topics and find the top K topics
   extractFeatures();
+  findMaxK(8);
+  console.log(topK);
+  datapoints = {};
 }
 
 function draw() {
-  noStroke();
-  fill('white');
+  x = width/2;
+  y = 170;
+  var i = 0;
 
-  console.log(datapoints);
+  textSize(30);
+  fill('white');
+  text("Top 20 Topics Showing up on NY Times Today", width/2, 50);
+
+  for (key in topK) {
+    rem = i % 4;
+    switch(rem) {
+      case 0: fill('#6A8A82'); break;
+      case 1: fill('#A37C27'); break;
+      case 2: fill('#A7414A'); break;
+      case 3: fill('#563838'); break;
+    }
+    setTextSize(topK[key]);
+    text(key, x, y);
+    y += 100;
+    i++;
+  }
+
+
 }
 
+function setTextSize(count) {
+  size = map(count, 0, maxValue, defaultTextSize, maxTextSize);
+  textSize(size);
+}
+
+//Returns topic associated to the given article count - returns the first one that matches the value
+//Source: https://stackoverflow.com/questions/9907419/how-to-get-a-key-in-a-javascript-object-by-its-value
+function getKeyByValue(value) {
+  return Object.keys(datapoints).find(key => datapoints[key] === value);
+}
+
+function findMaxK(k = 10) {
+  var arr = Object.values(datapoints);
+  maxValue = Math.max(...arr);
+  while (k > 0) {
+    //find max count
+    var max = Math.max(...arr);
+
+    //find topic with max article count
+    x = getKeyByValue(max);
+
+    //keep track of max count and associated topics for drawing
+    topK[x] = max;
+
+    //delete the max, helps to find next max
+    //Deleting from array: https://stackoverflow.com/questions/3954438/how-to-remove-item-from-array-by-value
+    var index = arr.indexOf(max);
+    if (index !== -1) arr.splice(index, 1);
+    delete datapoints[x];
+    k--;
+  }
+}
+
+//Rearranging JSON response from API into usable format
+//Extracting topics of articles from response and counting them
+var repeated = 0;
 function extractFeatures() {
 
   for (var i = 0; i < response.length; i++) {
@@ -45,6 +110,7 @@ function extractFeatures() {
       if (headline in headlines) {
         //if a headline is repeated, do not count its stats twice
         headlines[headline] += 1;
+        repeated++;
       } else {
         // if a new headline is encountered, count its stats
         headlines[headline] = 1;
@@ -61,5 +127,10 @@ function extractFeatures() {
         }
       }
     }
+  }
+  min_count = 5;
+  for (key in datapoints) {
+    if (datapoints[key] < min_count)
+      delete datapoints[key];
   }
 }
