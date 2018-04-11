0ecbb2a2cf4ac988b928c5b003145b6de771ac71
Pankhuri Kumar <pankhurikumar23@gmail.com>
2018-04-11 15:58:24 -0400

    A7 updated

--- assignment7/js/interactive1.js
+++ assignment7/js/interactive1.js
@@ -1,5 +1,4 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicGFua2h1cmlrdW1hciIsImEiOiJjamZwbnV2OTcxdXB1MzBudnViY2p3aDEzIn0.Zf9ZkY05gz_Zsyen1W1FbA';
-moving = false;
 
 var map = new mapboxgl.Map({
 	container: 'map',
@@ -9,33 +8,45 @@ var map = new mapboxgl.Map({
 map.on('load', function() {
 
 	var layers = ['0-100', '100-300', '300-500', '500-1000', '1000-9000', '9000+'];
-	var colors = ['#EABD5D', '#CB5B5A', '#AC557A', '#8D4C7D', '#6B406E', '#40324F'];
+	var colors = ['#feebe2', '#fcc5c0', '#fa9fb5', '#f768a1', '#c51b8a', '#7a0177'];
 
 	for (i = 0; i < layers.length; i++) {
 		var layer = layers[i];
 		var color = colors[i];
 		var item = document.createElement('div');
-	var key = document.createElement('span');
-	key.className = 'legend-key';
-	key.style.backgroundColor = color;
+		var key = document.createElement('span');
+		key.className = 'legend-key';
+		key.style.backgroundColor = color;
 
-	var value = document.createElement('span');
-	value.innerHTML = layer;
-	item.appendChild(key);
-	item.appendChild(value);
-	legend.appendChild(item);
+		var value = document.createElement('span');
+		value.innerHTML = layer;
+		item.appendChild(key);
+		item.appendChild(value);
+		legend.appendChild(item);
 	}
 });
 
 map.on('mousemove', function(e) {
-	var states = map.queryRenderedFeatures(e.point, {
-		layers: ['indianstates']
-	});
+	var states = map.queryRenderedFeatures(e.point, {layers: ['indianstates']});
 
 	if (states.length > 0) {
 		document.getElementById('pd').innerHTML = '<h3><strong>' + states[0].properties.NAME + '</strong></h3><p><strong><em>' + states[0].properties.density + '</strong> people per square kilometre</em></p>';
-		// moving = true;
+		displayGraphs(states[0].properties.NAME);
 	} else {
 		document.getElementById('pd').innerHTML = '<p>Hover over a state!</p>';
 	}
 });
+
+function displayGraphs(stateName) {
+	var item = document.getElementById('stateName');
+	item.innerHTML = stateName;
+
+	var canvas = document.getElementById("literacy");
+	var ctx = canvas.getContext("2d");
+	if (stateName == 'Uttar Pradesh') {
+		ctx.fillStyle = "#FF0000";
+	} else {
+		ctx.fillStyle = "#FF00FF";
+	}
+	ctx.fillRect(0,0,150,75);
+}
--- assignment7/js/interactive2.js
+++ assignment7/js/interactive2.js
@@ -1,11 +1,12 @@
-stateLabels = []; imr= []; noMethod = []; otherMethod = []; femaleSter = []; iud = []; pill = []; condom = []; relStart = [];
-textColours = ['#82672A','#8E732F','#9A7F35','#A68C3A','#B29940','#C8C8C8','#BEA646','#C9B44C','#D4C252','#DFD059','#EADE60'];
+stateLabels = []; aCare= []; noMethod = []; otherMethod = []; femaleSter = []; iud = []; pill = []; condom = []; relStart = [];
+textColour = ['#e572b1', '#f7f7f7', '#8fd14d'];
 barColours = ['#1A9850','#91CF60','#D9EF8B','#FEE08B','#FC8D59','#D73027'];
 finalX = 50; initialX = []; finalY = []; initialY = 600;
 maxAnim = 40; currAnim = 0; fl = -10;
 stateCount = 11;
 methodCount = 6;
 barStart = 330; barEnd = 1820; barWidth = barEnd - barStart;
+stateData = [];
 
 function preload() {
 	view2Data = loadTable('assets/View2.csv', 'csv', 'header');
@@ -25,15 +26,39 @@ function draw() {
 	textStyle(BOLD);
 
 	createAnimation();
+
 	if (fl > 45) {
 		drawBars();
+	} 
+	if (fl > 60){
+		fill('white');
+		textSize(30);
+		if (fl < 140) {
+			text('The relationship between antenatal care and infant mortality rate is clear', 330, 650);
+			text('... but what factors determine if a mother will get antenatal care?', 380, 690);
+		} else if (fl < 260) {
+			text('Awareness about Health Practices could be one indicator...', 450, 670);
+		} else if (fl < 320) {
+			text('Here we compare Family Planning Methods and Antenatal Care in rural India', 350, 670);
+		} else {
+			fill('#C8C8C8');
+			text('It\'s quite clear that states where women are more aware about safe sex', 380, 670);
+			text('are states where women are willing to receive better health care during pregnancy', 310, 710);
+		}
+		if (fl > 380) {
+			fill('white');
+			textSize(20);
+			text('Hover over the bars to explore the data', 650, 770);
+		}
 	}
-
+	if (fl > 400) {
+		createLegend();
+	}
 }
 
 function loadData() {
 	stateLabels = view2Data.getColumn("States");
-	imr = view2Data.getColumn("IMR");
+	aCare = view2Data.getColumn("Antenatal Care");
 	noMethod = view2Data.getColumn("No Method");
 	otherMethod = view2Data.getColumn("Other Methods");
 	femaleSter = view2Data.getColumn("Female Sterilization");
@@ -42,34 +67,41 @@ function loadData() {
 	condom = view2Data.getColumn("Condom");
 
 	for (var i = 0; i < stateCount; i++) {
-		noMethod[i] = map(noMethod[i], 0, 100, 0, barWidth - 3);
-		otherMethod[i] = map(otherMethod[i], 0, 100, 0, barWidth - 3);
-		femaleSter[i] = map(femaleSter[i], 0, 100, 0, barWidth - 3);
-		iud[i] = map(iud[i], 0, 100, 0, barWidth - 3);
-		pill[i] = map(pill[i], 0, 100, 0, barWidth - 3);
-		condom[i] = map(condom[i], 0, 100, 0, barWidth - 3);
+		stateData[i] = [];
+		stateData[i][0] = map(femaleSter[i], 0, 100, 0, barWidth - 3);
+		stateData[i][1] = map(iud[i], 0, 100, 0, barWidth - 3);
+		stateData[i][2] = map(pill[i], 0, 100, 0, barWidth - 3);
+		stateData[i][3] = map(condom[i], 0, 100, 0, barWidth - 3);
+		stateData[i][4] = map(otherMethod[i], 0, 100, 0, barWidth - 3);
+		stateData[i][5] = map(noMethod[i], 0, 100, 0, barWidth - 3);
 	}
 }
 
 function createAnimation() {
 	for(var i = 0; i < stateCount; i++) {
-		fill(textColours[i]);
 		finalY[i] = map(i, 0, 11, 50, 600);
+		if (i < 5) {
+			fill(textColour[0]);
+		} else if (i == 5) {
+			fill(textColour[1]);
+		} else {
+			fill(textColour[2]);
+		}
 		if (isAnimating()) {
 			x = finalX + (initialX[i] - finalX) * percAnim();
 			y = finalY[i] + (initialY - finalY[i]) * percAnim();
 			text(stateLabels[i], x, y);
-			text(imr[i], x, y + 20);
+			text(aCare[i] + '%', x, y + 20);
 		} else {
 			if (fl <= 0) {
 				x = map(i, 0, 11, 30, 1820);
 				initialX[i] = x;
 				text(stateLabels[i], initialX[i], initialY);
-				text(imr[i], initialX[i], initialY + 20);
+				text(aCare[i] + '%', initialX[i], initialY + 20);
 			} else {
 				noStroke();
 				text(stateLabels[i], finalX, finalY[i]);
-				text(imr[i], finalX, finalY[i] + 20);
+				text(aCare[i] + '%', finalX, finalY[i] + 20);
 			}
 		}
 	}
@@ -97,36 +129,91 @@ function percAnim() {
 function drawBars() {
 	stroke('#1A1A1A');
 	for (var i = 0; i < stateCount; i++) {
+		relStart[i] = [];
+		
 		k = 0;
-		relStart[i] = barStart;
-		addInfoBar(k, femaleSter[i], i);
-		addBlackBar(i);
+		relStart[i][k] = barStart;
+		addInfoBar(k, relStart[i][k], stateData[i][k], i);
+		addBlackBar(i, relStart[i][k] + stateData[i][k]);
+		
 		k++;
-		addInfoBar(k, iud[i], i);
-		addBlackBar(i);
+		relStart[i][k] = relStart[i][k-1] + stateData[i][k-1];
+		relStart[i][k] += 0.5;
+		addInfoBar(k, relStart[i][k], stateData[i][k], i);
+		addBlackBar(i, relStart[i][k] + stateData[i][k]);
+
 		k++;
-		addInfoBar(k, pill[i], i);
-		addBlackBar(i);
+		relStart[i][k] = relStart[i][k-1] + stateData[i][k-1];
+		relStart[i][k] += 0.5;
+		addInfoBar(k, relStart[i][k], stateData[i][k], i);
+		addBlackBar(i, relStart[i][k] + stateData[i][k]);
+
 		k++;
-		addInfoBar(k, condom[i], i);
-		addBlackBar(i);
+		relStart[i][k] = relStart[i][k-1] + stateData[i][k-1];
+		relStart[i][k] += 0.5;
+		addInfoBar(k, relStart[i][k], stateData[i][k], i);
+		addBlackBar(i, relStart[i][k] + stateData[i][k]);
+
 		k++;
-		addInfoBar(k, otherMethod[i], i);
-		addBlackBar(i);
+		relStart[i][k] = relStart[i][k-1] + stateData[i][k-1];
+		relStart[i][k] += 0.5;
+		addInfoBar(k, relStart[i][k], stateData[i][k], i);
+		addBlackBar(i, relStart[i][k] + stateData[i][k]);
+		
 		k++;
+		relStart[i][k] = relStart[i][k-1] + stateData[i][k-1];
+		relStart[i][k] += 0.5;
 		fill(barColours[k]);
-		rect(relStart[i], finalY[i] - 15, barEnd - relStart[i], 30, 3);
+		rect(relStart[i][k], finalY[i] - 15, barEnd - relStart[i][k], 30, 3);
+		relStart[i][k] = relStart[i][k-1] + stateData[i][k-1];
+	}
+
+	checkBubble();
+}
+
+function addInfoBar(k, start, info, i) {
+	fill(barColours[k]);
+	rect(start, finalY[i] - 15, start + info, 30, 3);
+}
+
+function addBlackBar(i, start) {
+	fill('#1A1A1A');
+	rect(start, finalY[i] - 15, start + 0.5, 30, 3);
+}
+
+function checkBubble() {
+	for (var i = 0; i < stateCount; i++) {
+		if (abs(mouseX - relStart[i][0]) < stateData[i][0] && abs(relStart[i][0] + stateData[i][0] - mouseX) < stateData[i][0] && abs(mouseY - (finalY[i])) < 15) {
+			createBubble('Female Sterilization', femaleSter[i]);
+		} else if (abs(mouseX - relStart[i][1]) < stateData[i][1] && abs(relStart[i][1] + stateData[i][1] - mouseX) < stateData[i][1] && abs(mouseY - (finalY[i])) < 15) {
+			createBubble('IUD/PPIUD', iud[i]);
+		} else if (abs(mouseX - relStart[i][2]) < stateData[i][2] && abs(relStart[i][2] + stateData[i][2] - mouseX) < stateData[i][2] && abs(mouseY - (finalY[i])) < 15) {
+			createBubble('Pill', pill[i]);
+		} else if (abs(mouseX - relStart[i][3]) < stateData[i][3] && abs(relStart[i][3] + stateData[i][3] - mouseX) < stateData[i][3] && abs(mouseY - (finalY[i])) < 15) {
+			createBubble('Condom', condom[i]);
+		} else if (abs(mouseX - relStart[i][4]) < stateData[i][4] && abs(relStart[i][4] + stateData[i][4] - mouseX) < stateData[i][4] && abs(mouseY - (finalY[i])) < 15) {
+			createBubble('Other Methods', otherMethod[i]);
+		} else if (abs(mouseX - relStart[i][5]) < stateData[i][5] && abs(relStart[i][k] + stateData[i][5] - mouseX) < stateData[i][5] && abs(mouseY - (finalY[i])) < 15) {
+			createBubble('No Family Planning', noMethod[i]);
+		}
 	}
 }
 
-function addInfoBar(k, info, i) {
-	fill(barColours[k]);
-	rect(relStart[i], finalY[i] - 15, relStart[i] + info, 30, 3);
-	relStart[i] += info;
+function createBubble(textType, info) {
+	noStroke();
+	fill(255, 255, 255, 204);
+	rect(mouseX+5, mouseY-30, textWidth(textType + ": " + info + '%') + 5, 20);
+	fill(0);
+	text(textType + ": " + info + '%', mouseX + 7, mouseY - 15);
+	stroke('#1A1A1A');
 }
 
-function addBlackBar(i) {
-	fill('#1A1A1A');
-	rect(relStart[i], finalY[i] - 15, relStart[i] + 0.5, 30, 3);
-	relStart[i] += 0.5;
+function createLegend() {
+	textSize(15);
+	fill(textColour[0]);
+	text('States with lowest antenatal care', 450, 800);
+	text('% of women that had at least 4 antenatal care visits', 450, 820);
+	fill(textColour[2]);
+	text('States with highest antenatal care', 920, 800);
+	text('% of women that had at least 4 antenatal care visits', 920, 820);
 }
