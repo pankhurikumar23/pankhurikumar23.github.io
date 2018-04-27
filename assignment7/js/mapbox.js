30b2b6a431140470339e743b976f0ab405c4e36a
Pankhuri Kumar <pankhurikumar23@gmail.com>
2018-04-26 23:57:20 -0400

    reverting

--- assignment7/js/mapbox.js
+++ assignment7/js/mapbox.js
@@ -5,44 +5,23 @@ var map = new mapboxgl.Map({
 	style: 'mapbox://styles/pankhurikumar/cjfox2e133gle2rn0qjvi48wj'
 });
 
+function getStates() {
+	var mySource = map.getLayer('indianstates');
+	console.log(mySource);
+	return mySource;
+}
+
 map.on('load', function() {
-	// map.addSource("states", {
- //        "type": "geojson",
- //        "data": "assets/india_states.geojson"
- //    });
-
-	// map.addLayer({
- //        "id": "state-fills-hover",
- //        "type": "fill",
- //        "source": "states",
- //        "layout": {},
- //        "paint": {
- //            "fill-color": "#627BC1",
- //            "fill-opacity": 1
- //        },
- //        "filter": ["==", "name", ""]
- //    });
-
     map.on('mousemove', function(e) {
-		var state = map.queryRenderedFeatures(e.point, {layers: ['indianstates']});
+		var states = map.queryRenderedFeatures(e.point, {layers: ['indianstates']});
 
 		if (states.length > 0) {
 			document.getElementById('pd').innerHTML = '<h3><strong>' + states[0].properties.NAME + '</strong></h3><p><strong><em>' + states[0].properties.density + '</strong> people per square kilometre</em></p>';
-			displayName(state[0].properties.NAME);
+			displayName(states[0].properties.NAME);
 		} else {
 			document.getElementById('pd').innerHTML = '<p>Hover over a state!</p>';
 		}
 	});
-
-	// map.on("mousemove", "state-fills", function(e) {
-	// 	var state = map.queryRenderedFeatures(e.point, {layers: ['indianstates']});
- //        map.setFilter("state-fills-hover", ["==", "name", state[0].properties.name]);
- //    });
-
- //    // Reset the state-fills-hover layer's filter when the mouse leaves the layer.
- //    map.on("mouseleave", "state-fills", function() {
- //        map.setFilter("state-fills-hover", ["==", "name", ""]);
- //    });
 
 	var layers = ['0-100', '100-300', '300-500', '500-1000', '1000-9000', '9000+'];
 	var colors = ['#f1eef6','#d0d1e6','#a6bddb','#74a9cf','#2b8cbe','#045a8d'];
